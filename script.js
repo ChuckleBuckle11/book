1d314d33cd5665f1875694be3b8cf5fc54550a8f
Dylan <102398736+ChuckleBuckle11@users.noreply.github.com>
2022-11-05 15:18:48 +0800

    wow I added a lot of shit, should commit more

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ displayBooks();
 // when clicking the Submit button, add a new Book
 const submitButton = document.getElementById("submitButton");
 const form = document.querySelector("#form");
-console.log(submitButton);
 submitButton.onclick = () => {
 
   let formElements = form.elements;
@@ -36,7 +35,7 @@ submitButton.onclick = () => {
 
   let newBook = new Book(author,title,year,numberOfPages,read)
   addBookToLibrary(newBook);
-  console.log(myLibrary);
+  displayBooks();
 
 }
 
@@ -56,54 +55,93 @@ function addBookToLibrary(book) {
   // do stuff here
 }
 
-function showLibrary() {
-  console.table(myLibrary);
-}
+
+
+
 
 function displayBooks() {
   const library = document.getElementsByClassName("library")[0];
 
   // removes any existing items 
-  const items = document.querySelectorAll('.item');
+  const items = document.querySelectorAll('.item-row');
   items.forEach(item => {
     item.remove();
   });
 
-
+  let id = 1;
   for (book of myLibrary){
     //Creates new row to be appended to library
     const newRow = document.createElement("div");
     //creates listing 
+    const book_id = document.createElement("div");
+    book_id.textContent = id;
+    book_id.className = "id";
     const author = document.createElement("div");
     author.textContent = book.author;
-    author.className = "item"
+    author.className = "author"
     const title = document.createElement("div");
     title.textContent = book.title;
-    title.className = "item"
+    title.className = "title"
     const year = document.createElement("div");
     year.textContent = book.year;
-    year.className = "item"
+    year.className = "year"
     const numberofPages = document.createElement("div");
     numberofPages.textContent = book.numberOfPages;
-    numberofPages.className = "item"
+    numberofPages.className = "numberOfPages"
     const read = document.createElement("div");
     read.textContent = book.read;
-    read.className = "item"
+    read.className = "read"
+
+    //creates delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "deleteButton";
+    deleteButton.id = id;
+    deleteButton.onclick = () => {
+      deleteRow();
+    };
+
+
+
+    //creates ChangeReadStatus button
+    const readStatusButton = document.createElement("button");
+    readStatusButton.textContent = "Read?";
+    readStatusButton.className = "readStatusButton";
+    readStatusButton.id = id;
+    readStatusButton.onclick = () => {
+      changeReadStatus();
+    };
 
     //appends items to the new row
-    newRow.append(author,title,year,numberofPages,read);
+    newRow.append(book_id,author,title,year,numberofPages,read,deleteButton,readStatusButton);
 
     //sets newRow styles to grid
     newRow.style.display = "grid";
-    newRow.style["grid-template-columns"] = "20% 20% 20% 20% 20%";
+    newRow.className = "item-row";
 
     //appends row to library
     library.appendChild(newRow);
-
+    id++;
 
   }
 }
 
-function clearForm(){
+function deleteRow() {
+  // event.target will be the input element.
+  var td = event.target; 
+  var tr = td.parentNode; // the row to be removed
+  tr.parentNode.removeChild(tr);
+}
 
+function changeReadStatus(){
+  let td = event.target;
+  let tr = td.parentNode;
+  let readStatus = tr.getElementsByClassName("read")[0].textContent;
+  if (readStatus == "Read"){
+    tr.getElementsByClassName("read")[0].textContent = "Not Read";
+    return 
+  }
+  tr.getElementsByClassName("read")[0].textContent = "Read";
+
+  return 
 }
